6265550de1af86202f68d24a1d3968b0c555be2a
Paulo Walraven <pmwc04@gmail.com>
2022-07-27 15:06:29 -0300

    fix return of handle_insert

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -8,9 +8,7 @@ defmodule Rockelivery.Users.Create do
     |> handle_insert()
   end
 
-  defp handle_insert({:ok, %User{} = result}), do: result
+  defp handle_insert({:ok, %User{} = result}), do: {:ok, %User{} = result}
 
-  defp handle_insert({:error, result}) do
-    {:error, %{status: :bad_request, result: result}}
-  end
+  defp handle_insert({:error, result}), do: {:error, %{status: :bad_request, result: result}}
 end
