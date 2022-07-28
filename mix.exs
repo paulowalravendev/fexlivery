497c66ad56f50bc91c9e8053b3fb990c69df1f86
Paulo Walraven <pmwc04@gmail.com>
2022-07-28 00:15:28 -0300

    add coveralls

--- mix.exs
+++ mix.exs
@@ -9,7 +9,14 @@ defmodule Rockelivery.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -45,7 +52,8 @@ defmodule Rockelivery.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:pbkdf2_elixir, "~> 2.0"}
+      {:pbkdf2_elixir, "~> 2.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
